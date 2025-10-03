0eb54a56d5f67579b84f7bda1e3d563778918a43
Lazar357 <lazar.danic.23@singimail.rs>
2025-10-03 15:50:17 +0200

    Add utils

--- src/app/profile/profile.ts
+++ src/app/profile/profile.ts
@@ -1,7 +1,9 @@
 import { Component, signal } from '@angular/core';
 import { UserModel } from '../../models/user.model';
-import { Router } from '@angular/router';
 import { UserService } from '../../services/user.service';
+import { Router } from '@angular/router';
+import { ReservationModel } from '../../models/reservation.model';
+import { Utils } from '../utils';
 
 @Component({
   selector: 'app-profile',
@@ -10,15 +12,35 @@ import { UserService } from '../../services/user.service';
   styleUrl: './profile.css'
 })
 export class Profile {
-
   protected currentUser = signal<UserModel | null>(null)
 
-  constructor(private router: Router){
-
+  constructor(private router: Router, public utils: Utils) {
     try {
       this.currentUser.set(UserService.getActiveUser())
     } catch {
+      // Nema aktivnog korisnika
+      // Idi na login
       this.router.navigate(['/login'])
     }
+  }
+
+  protected pay(r: ReservationModel) {
+    UserService.updateReservationStatus(r.createdAt, 'paid')
+    this.currentUser.set(UserService.getActiveUser())
+  }
+
+  protected cancel(r: ReservationModel) {
+    UserService.updateReservationStatus(r.createdAt, 'canceled')
+    this.currentUser.set(UserService.getActiveUser())
+  }
+
+  protected like(r: ReservationModel) {
+    UserService.updateReservationStatus(r.createdAt, 'liked')
+    this.currentUser.set(UserService.getActiveUser())
+  }
+
+  protected dislike(r: ReservationModel) {
+    UserService.updateReservationStatus(r.createdAt, 'disliked')
+    this.currentUser.set(UserService.getActiveUser())
   }
 }
